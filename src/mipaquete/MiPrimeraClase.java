f69e33bdda0398df9f42b3051a07ec2953d8a743
Walter Meza <walter.mezaalmeida@griffithuni.edu.au>
2018-08-15 20:41:50 +1000

    Array four dimensions and double

---
+++ src/mipaquete/MiPrimeraClase.java
@@ -0,0 +1,26 @@
+
+package mipaquete;
+public class MiPrimeraClase {
+    public static void main(String[] args) {
+        
+        //byte b=4;
+        //short s=b;
+        //b(byte) s;
+        
+        char[] names = new char[3];
+        names[0]= 'h';
+        names[1]= 'o';
+        names[2]= 'l';
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        
+        char[][][][] monkey= new char[2][3][2][2];
+        monkey[1][0][0][1]= 'm';
+        
+        
+        System.out.println(monkey[1][0][0][1]);
+        
+    }
+    
+}
